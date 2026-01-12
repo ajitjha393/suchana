083eeb7ecfb1a6c16146a18b42ff7818ff69bb3b
ajitjha393 <ajitjha393@gmail.com>
2026-01-12 18:51:28 +0530

    `chore`: set up Angular Material with theming

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NotificationsPageComponent } from './pages/notifications/notifications-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'notifications', pathMatch: 'full' },
+  { path: 'notifications', component: NotificationsPageComponent },
+  { path: '**', redirectTo: 'notifications' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
