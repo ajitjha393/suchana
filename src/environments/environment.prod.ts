5b9a49d37ce9c5015b31c9a8fc9707c75d865e57
ajitjha393 <ajitjha393@gmail.com>
2026-01-19 14:33:12 +0530

    `refactor(http)`: simplify API config and use environment variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  apiBaseUrl: 'https://your-prod-domain.com/api', // change this later to prod API URL
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+    apiBaseUrl: '/api', // dev uses proxy -> http://localhost:4000
+
 };
 
 /*
